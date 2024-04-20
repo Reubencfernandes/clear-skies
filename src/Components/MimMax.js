4c00c825261a3cf8f410ad8cf13f4bf1a3bd55ae
Reubencfernandes <18reuchagasfernandes@gmail.com>
2024-04-20 12:40:44 +0530

    adding files

---
+++ src/Components/MimMax.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MimMax = () => {
+  return (
+    <div>
+      <p>Feels like 13°C</p>
+      <p>Maximum Temperature : 20°C</p>
+      <p>Minimum Temperature : 20°C</p>
+    </div>
+  )
+}
+
+export default MimMax
