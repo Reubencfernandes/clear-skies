e077e97d88f6d7a9bbbdfc3c1066576417dab372
Reubencfernandes <18reuchagasfernandes@gmail.com>
2024-04-30 10:48:14 +0530

    currentweather done

--- website/src/Components/CurrentWeather.js
+++ website/src/Components/CurrentWeather.js
@@ -4,25 +4,25 @@ import { WeatherIcon } from 'weather-react-icons'
 import Flag from 'react-flagkit';
 const CurrentWeather = () => {
     return (
-        <div className='bg-hm text-white rounded-3xl bg-cover m-4 font-body'>
+        <div className='bg-hm text-white rounded-3xl bg-contain m-4 font-body'>
             <div className='flex justify-between p-5'>
                 <div className='flex'>
                     <WeatherIcon iconId={200} name="owm" />
-                    <p className='pl-2'>Temperature</p>
+                    <p className='pl-2 text-md'>Temperature</p>
                 </div>
                 <div>
                     <h1 className='font-bebas text-3xl'>20 MAY</h1>
                     <h1 className='font-bebas'>11:22 PM</h1>
                 </div>
             </div>
-            <div>
+            <div className='p-2'>
                 <div className='flex p-1'>
-            <Flag country="US" />
-            <h1 className='font-bebas'>London</h1>
+            <Flag country="GB" />
+            <h1 className='font-bebas text-5xl pl-2'>London</h1>
                 </div>
-            <p className='font-bebas'> -0.1152, 51.5005</p>
-            <h1 className='font-bebas'>10°C</h1>
-            <p>Broken Clouds</p>
+            <p className='font-bebas ml-10 text-xl'> -0.1152, 51.5005</p>
+            <h1 className='font-bebas text-4xl ml-10'>10°C</h1>
+            <p className='ml-10 text-md'>Broken Clouds</p>
             </div>
         </div> 
     )
